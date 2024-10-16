d2cb322448ce0f4dbaaa8e65cf50ea5a396e12c7
luisp <luisp@TAKISZOMBI>
2024-10-16 17:49:35 -0600

    Se creó el servicio de Registrar usuario, sus funciones todavía no están implementadas

---
+++ Servicios/IServicioRegistrarUsuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Servicios
+{
+    [ServiceContract]
+    public interface IServicioRegistrarUsuario
+    {
+        bool ValidarDatos(Usuario usuario);
+        bool ValidarCorreo(string codigoCorreo);
+        bool EnviarCodigoCorreo(string correo);
+        bool RegistrarUsuario(Usuario usuario);
+    }
+
+    [DataContract]
+    public class Usuario
+    {
+        private string nombreUsuario;
+        private string contrasenia;
+        private string correo;
+
+       
+
+        [DataMember]
+        public string NombreUsuario { get { return nombreUsuario; } set { nombreUsuario = value; } }
+
+        [DataMember]
+        public string Contrasenia { get { return contrasenia; } set { contrasenia = value; } }
+
+        [DataMember]
+        public string Correo { get { return correo; } set { correo = value; } }
+    }
+}
---
+++ Servicios/ServicioRegistrarUsuario.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Servicios
+{
+    public class ServicioRegistrarUsuario : IServicioRegistrarUsuario
+    {
+        public bool EnviarCodigoCorreo(string correo)
+        {
+            /*
+             * Función que envía el código al correo ingresado
+             * debería hacer una validación para que no se haga spam
+             */
+            Console.WriteLine("Se envía el código al correo: ", correo);
+
+            return true;
+        }
+
+        public bool RegistrarUsuario(Usuario usuario)
+        {
+            /*
+             * Función que guarda el usuario en la base de datos
+             */
+            Console.WriteLine("añadiendo al usuario con nombre: ", usuario.NombreUsuario, " y contraseña: ", usuario.Contrasenia, "...");
+            
+
+            return true;
+
+        }
+
+        public bool ValidarCorreo(string codigoCorreo)
+        {
+            /*
+             * Función que valida si el código ingresado
+             * es el mismo enviado al correo
+             */
+            Console.WriteLine("Se valida que el código ingresado sí es el mismo que el enviado al correo");
+
+            return true;
+        }
+
+        public bool ValidarDatos(Usuario usuario)
+        {
+            /*
+             * Función que valida si los datos nombre de usuario, contrasenia
+             * correo cumplen el formato y si el correo existe
+             */
+            Console.WriteLine("Se valida que los datos ingresados cumplen formato y el correo existe");
+
+            return true;
+        }
+
+    }
+}
