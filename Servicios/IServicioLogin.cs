cc87658bb64c5ed1d2f5fe88c2948a5568a76410
Saúl Barragán <barragansaul26n@gmail.com>
2024-10-15 23:55:36 -0600

    Se agregaron algunas cosas relacionadas al servicio de Login, falta soluciona problemas de DBContext

---
+++ Servicios/IServicioLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    [ServiceContract]
+    internal interface IServicioLogin
+    {
+        [OperationContract]
+        bool Login(string nombreUsuario, string contrasenia);
+    }
+}
---
+++ Servicios/ImplementacionLogin.cs
@@ -0,0 +1,20 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    internal class ImplementacionLogin : IServicioLogin
+    {
+        public bool Login(string nombreUsuario, string contrasenia)
+        {
+            using (var contexto = new ModeloDBContainer())
+            {
+
+            }
+        }
+    }
+}
