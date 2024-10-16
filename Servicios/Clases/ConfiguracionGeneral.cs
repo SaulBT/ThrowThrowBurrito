e4160b209edc337e660cb11afbcfd62bb74bbcd5
Saúl Barragán <barragansaul26n@gmail.com>
2024-10-15 18:50:22 -0600

    Las clase Jugador se corrigió y la clase ConfiguraciónGeneral se terminó (nota: esta tal vez vaya en el cliente).

---
+++ Servicios/Clases/ConfiguracionGeneral.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Clases
+{
+    public class ConfiguracionGeneral
+    {
+        private int volumen;
+        private int volumenMusica;
+        private int volumenSonidos;
+        private int brillo;
+        private int sensibilidad;
+        private String idioma;
+
+        public ConfiguracionGeneral()
+        {
+
+        }
+
+        public ConfiguracionGeneral(int volumen, int volumenMusica, int volumenSonidos, int brillo, int sensibilidad, string idioma)
+        {
+            this.volumen = volumen;
+            this.volumenMusica = volumenMusica;
+            this.volumenSonidos = volumenSonidos;
+            this.brillo = brillo;
+            this.sensibilidad = sensibilidad;
+            this.idioma = idioma;
+        }
+
+        public int Volumen
+        {
+            get { return volumen; }
+            set { volumen = value; }
+        }
+
+        public int VolumenMusica
+        {
+            get { return volumenMusica; }
+            set { volumenMusica = value; }
+        }
+
+        public int VolumenSonidos
+        {
+            get { return volumenSonidos; }
+            set { volumenSonidos = value; }
+        }
+
+        public int Brillo
+        {
+            get { return brillo; }
+            set { brillo = value; }
+        }
+
+        public int Sensibilidad
+        {
+            get { return sensibilidad; }
+            set { sensibilidad = value; }
+        }
+
+        public String Idioma
+        {
+            get { return idioma; }
+            set { idioma = value; }
+        }
+    }
+}
