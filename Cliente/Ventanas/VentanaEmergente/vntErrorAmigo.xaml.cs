d55a97107639efed0b0edb3a877e0a36e3138a22
Saúl Barragán <barragansaul26n@gmail.com>
2024-11-10 14:49:51 -0600

    Las ventanas están creadas pero falta todo el código, todo el sabor pues

---
+++ Cliente/Ventanas/VentanaEmergente/vntErrorAmigo.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cliente.Ventanas.VentanaEmergente
+{
+    /// <summary>
+    /// Interaction logic for vntErrorAmigo.xaml
+    /// </summary>
+    public partial class vntErrorAmigo : UserControl
+    {
+        public vntErrorAmigo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptarEmergente_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Cliente/Ventanas/VentanaEmergente/vntNotificaciones.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cliente.Ventanas.VentanaEmergente
+{
+    /// <summary>
+    /// Interaction logic for vntErrorAmigo.xaml
+    /// </summary>
+    public partial class vntNotificaciones : UserControl
+    {
+        public vntNotificaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRechazar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVolver_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
